db99b071f5ca3175e2c5432af3326692cec85c01
Mateleo <mateleo78@gmail.com>
2024-10-17 15:05:05 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+// When adding VueUse as a nuxt module, vueusecore is not added
+
+const {height} = useWindowSize()
+</script>
+<template>
+  <div>
+    {{ height }}
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
